6616ff27dbf3d61d169eb31790a911f29d07c296
jeremyvnt <jervincent@free.fr>
2015-12-02 13:54:56 +0100

    Ajout des commentaires pour StatsService + Ajout des tests

--- src/main/java/fr/synapsegaming/stats/service/StatsService.java
+++ src/main/java/fr/synapsegaming/stats/service/StatsService.java
@@ -2,22 +2,56 @@ package fr.synapsegaming.stats.service;
 
 import java.util.List;
 
-import fr.synapsegaming.media.entity.Article;
 import fr.synapsegaming.user.entity.Clazz;
 import fr.synapsegaming.user.entity.Race;
 import fr.synapsegaming.user.entity.Specialization;
 import fr.synapsegaming.user.entity.User;
 
+/**
+ * <b>StatsService</b> is the public interface for Stats
+ * 
+ * @author Patrois_Vincent
+ * 
+ */
 public interface StatsService {
 	
+	/**
+     * List the top 5 clazzs most played
+     * 
+     * @return a list of Clazz
+     */
 	public List<Clazz> getFiveClazzMostPlayed();
 	
+	
+	/**
+     * List the top 5 races most played
+     * 
+     * @return a list of Race
+     */
 	public List<Race> getFiveRaceMostPlayed();
 	
+	
+	/**
+     * List the top 5 specialization most played
+     * 
+     * @return a list of Specialization
+     */
 	public List<Specialization> getFiveSpecializationMostPlayed();
 	
+	
+	/**
+     * List all the users without Avatar
+     * 
+     * @return a list of User
+     */
 	public List<User> getUsersWithoutAvatar();
 	
+	
+	/**
+     * List the top 5 most active users
+     * 
+     * @return a list of User
+     */
 	public List<User> getFiveUsersMostActive();
 	
 	
--- src/test/java/fr/synapsegaming/stats/service/StatsServiceTest.java
+++ src/test/java/fr/synapsegaming/stats/service/StatsServiceTest.java
@@ -1,20 +1,41 @@
 package fr.synapsegaming.stats.service;
 
 import static org.junit.Assert.assertTrue;
-
 import org.apache.commons.collections.CollectionUtils;
 import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 
-import fr.synapsegaming.utils.CipherUtils;
+import fr.synapsegaming.commons.service.AbstractServiceTest;
 
-public class StatsServiceTest {
+public class StatsServiceTest extends AbstractServiceTest {
+	
+	private static final int TOP_5 = 5;
+	
+	@Autowired
+	StatsService statsService;
+	
 	@Test
-    public void testListClassMostPlayed() {
-	//	assertT
+    public void testGetFiveClazzMostPlayed() {
+		assertTrue(statsService.getFiveClazzMostPlayed().size() == TOP_5);
     }
 	
 	@Test
-	public void testListForValidExtension() {
-		//assertTrue(CollectionUtils.isNotEmpty(raidDao.listForExtension(VALID_EXTENSION)));
-	}
+    public void testGetFiveRaceMostPlayed() {
+		assertTrue(statsService.getFiveRaceMostPlayed().size() == TOP_5);
+    }
+	
+	@Test
+    public void testGetSpecializationMostPlayed() {
+		assertTrue(statsService.getFiveSpecializationMostPlayed().size() == TOP_5);
+    }
+	
+	@Test
+    public void testGetUsersWithoutAvatar() {
+		assertTrue(CollectionUtils.isNotEmpty(statsService.getUsersWithoutAvatar()));
+    }
+	
+	@Test
+    public void testGetFiveUsersMostActive() {
+		assertTrue(statsService.getFiveUsersMostActive().size() == TOP_5);
+    }
 }
